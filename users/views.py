406338ca639cd60cbec64e9a3bd4366dfef1fb66
FlakoBB <josephrbb99@gmail.com>
2023-11-11 15:14:05 -0600

    add: new rute users_list

--- users/views.py
+++ users/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import User
 
 def profile(request):
   return render(request, 'users/perfil.html')
+
+def show_users(request):
+  users = User.objects.all()
+  return render(request, 'users/users-list.html', {'users': users})
